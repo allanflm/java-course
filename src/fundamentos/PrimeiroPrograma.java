4f03820ba5fa168a3577fc40fe1eb4728c4da5eb
Allan <allanfelipedk@gmail.com>
2024-03-07 08:22:17 -0300

    aula: primeiro programa (16-17)

---
+++ src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class PrimeiroPrograma 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Primeiro programa #1");
+		
+		
+		System.
+		out.
+		println("Segundo programa #2");
+	}	
+}
