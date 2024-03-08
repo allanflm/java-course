3a77b1df9b7f93e7743bd01706925ab71a0d022f
Allan <allanfelipedk@gmail.com>
2024-03-08 19:30:43 -0300

    a.20 - comentario em java

--- src/fundamentos/PrimeiroPrograma.java
+++ src/fundamentos/PrimeiroPrograma.java
@@ -1,14 +1,29 @@
 package fundamentos;
+/**
+ * @author allan
+ * @since 1.0
+ */
+public class PrimeiroPrograma {
+	public static void main(String[] args) {
+		/**
+		 * comentario JavaDoc 
+		 * @param
+		 */
 
-public class PrimeiroPrograma 
-{
-	public static void main(String[] args) 
-	{
+		// ctrl + / -> comenta as linhas
+
+//		// Uma sentença de codigo termina com ;
+//		System.out.println("Primeiro programa #1");
+
+//		/*
+//		 * comentariio de 
+//		 * varias
+//		 * linhas
+//		 * */
+//		
+//		System.out.println("Segundo programa #2");
+
 		System.out.println("Primeiro programa #1");
-		
-		
-		System.
-		out.
-		println("Segundo programa #2");
-	}	
+		System.out.println("Fim");// aqui acabou!
+	}
 }
